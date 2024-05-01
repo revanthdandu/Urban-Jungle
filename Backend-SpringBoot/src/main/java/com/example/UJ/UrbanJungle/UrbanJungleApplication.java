23a813c6711402b839b671224bfcc1aef4fb7f76
revanthdandu <revanth.dandu@klh.edu.in>
2024-05-02 00:13:03 +0530

    backend setup

---
+++ Backend-SpringBoot/src/main/java/com/example/UJ/UrbanJungle/UrbanJungleApplication.java
@@ -0,0 +1,13 @@
+package com.example.UJ.UrbanJungle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanJungleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanJungleApplication.class, args);
+	}
+
+}
---
+++ Backend-SpringBoot/src/test/java/com/example/UJ/UrbanJungle/UrbanJungleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UJ.UrbanJungle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanJungleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
